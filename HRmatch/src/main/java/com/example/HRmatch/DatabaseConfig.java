7b737dee313cb7666eca2f7ed951d4d76082c359
Морозова Екатерина <katyamoroz03@mail.ru>
2026-05-09 13:58:10 +0300

    Initial commit

---
+++ HRmatch/src/main/java/com/example/HRmatch/DatabaseConfig.java
@@ -0,0 +1,53 @@
+package com.example.HRmatch;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${DB_HOST:localhost}")
+    private String dbHost;
+
+    @Value("${DB_PORT:5432}")
+    private int dbPort;
+
+    @Value("${DB_NAME:testdb}")
+    private String dbName;
+
+    @Value("${DB_USER:sa}")
+    private String dbUser;
+
+    @Value("${DB_PASSWORD:}")
+    private String dbPassword;
+
+    @Value("${USE_POSTGRES:false}")
+    private boolean usePostgres;
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+
+        if (usePostgres) {
+            // Формат с параметрами: надёжно работает с любыми паролями
+            String url = String.format(
+                    "jdbc:postgresql://%s:%d/%s?user=%s&password=%s&ssl=true",
+                    dbHost, dbPort, dbName, dbUser, dbPassword
+            );
+            ds.setJdbcUrl(url);
+            ds.setDriverClassName("org.postgresql.Driver");
+        } else {
+            // Локальная H2 база
+            ds.setJdbcUrl("jdbc:h2:mem:testdb");
+            ds.setDriverClassName("org.h2.Driver");
+            ds.setUsername("sa");
+            ds.setPassword("");
+        }
+        return ds;
+    }
+}
