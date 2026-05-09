822e2469736cce471f0b9e15ab857548a8f0522e
Морозова Екатерина <katyamoroz03@mail.ru>
2026-05-09 18:34:33 +0300

    Initial commit

--- HRmatch/src/main/java/com/example/HRmatch/entity/CandidateCompetency.java
+++ HRmatch/src/main/java/com/example/HRmatch/entity/CandidateCompetency.java
@@ -1,25 +1,27 @@
 package com.example.HRmatch.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "candidate_competencies")
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CandidateCompetency {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JsonIgnore
     private User candidate;
 
     @ManyToOne
     private Competency competency;
 
-    @Min(1) @Max(5)
-    private Integer level; // уровень кандидата: 1-5
+    private Integer level;
 }
--- HRmatch/src/main/java/com/example/HRmatch/entity/VacancyCompetency.java
+++ HRmatch/src/main/java/com/example/HRmatch/entity/VacancyCompetency.java
@@ -1,25 +1,27 @@
 package com.example.HRmatch.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "vacancy_competencies")
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VacancyCompetency {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JsonIgnore
     private Vacancy vacancy;
 
     @ManyToOne
     private Competency competency;
 
-    @Min(1) @Max(5)
-    private Integer requiredLevel; // требуемый уровень: 1-5
+    private Integer requiredLevel;
 }
