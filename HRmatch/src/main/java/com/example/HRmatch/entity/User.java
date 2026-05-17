26b37458dd54dbc2b4a832d4ce6675d1bb3f800f
Морозова Екатерина <katyamoroz03@mail.ru>
2026-05-17 23:00:26 +0300

    Initial commit

--- HRmatch/src/main/java/com/example/HRmatch/entity/User.java
+++ HRmatch/src/main/java/com/example/HRmatch/entity/User.java
@@ -30,8 +30,8 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Lob
-    @Column(name = "profile_photo")
+    @Column(name = "profile_photo", columnDefinition = "bytea")
+    @Basic(fetch = FetchType.LAZY)
     private byte[] profilePhoto;
     public byte[] getProfilePhoto() { return profilePhoto; }
     public void setProfilePhoto(byte[] profilePhoto) { this.profilePhoto = profilePhoto; }
